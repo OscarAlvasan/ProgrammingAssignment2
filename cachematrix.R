fa4509d3bd580e19caa5016c612f9b9554c7a32c
Oscar Alvarado <64665580+OscarAlvasan@users.noreply.github.com>
2020-06-01 14:48:51 -0500

    Caching inersion of matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions cache the value of the inverse of a matrix
 
-## Write a short comment describing this function
+## This function creates a matrix which contains a function to set the value of
+## a matrix, get the value of the matrix, set the value of the inverse and get
+## the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This calculres the inverse of the matrix checked first if the inverse has
+## already been calculated if so, it gets the inverse from the cache and skips
+## the computation
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
 }
